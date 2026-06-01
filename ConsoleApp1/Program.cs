7a6cba44a8b8c88ccb7f28b0f1717d0f4b8c9b1e
tharindug99 <tharindunirmala99@gmail.com>
2026-06-01 09:56:17 +0530

    Add NuGet configuration and project assets for Newtonsoft.Json package
    - Created JsonApp.csproj.nuget.g.props and JsonApp.csproj.nuget.g.targets to manage NuGet package restoration.
    - Added project.assets.json to define package dependencies and assets for Newtonsoft.Json version 13.0.4.
    - Introduced project.nuget.cache to cache NuGet package information.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("What is your name");
+
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d}");
+Console.WriteLine($"{Environment.NewLine}Press Any Key to exit...");
+Console.ReadKey(true);
---
+++ JsonApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+}
+
+public class JsonApp
+{
+    public static void Main()
+    {
+        string json = "{\"Name\": \"John Doe\", \"Age\": 30}";
+        Person person = JsonConvert.DeserializeObject<Person>(json);
+        Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+    }
+}
